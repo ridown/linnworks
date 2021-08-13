a8da5a24861aa5ace17ff73bee9e277de4f08898
Paul <paul@shopapps.co.uk>
2021-08-13 15:38:15 +0100

    Update Stock.php

--- src/Api/Stock.php
+++ src/Api/Stock.php
@@ -5,6 +5,19 @@ namespace Onfuro\Linnworks\Api;
 class Stock extends ApiClient
 {
     public $path = 'Stock';
+    const StockLevels = 0;
+    const Pricing = 1;
+    const Supplier = 2;
+    const ShippingInformation = 3;
+    const ChannelTitle = 4;
+    const ChannelDescription = 5;
+    const ChannelPrice = 6;
+    const ExtendedProperties = 7;
+    const Images = 8;
+
+    const SKU = 0;
+    const Title = 1;
+    const Barcode = 2;
 
     public function getStockConsumption(string $stockItemId = "", string $locationId = "", string $startDate = "", string $endDate = "")
     {
@@ -15,21 +28,25 @@ class Stock extends ApiClient
             "endDate" => $endDate,
         ]);
     }
-    
-    public function getStockItemsFull(string $keyWord = "",string $locationId = "",int $entriesPerPage = 100, int $pageNumber = 1, bool $excludeComposites = true, bool $excludeVariations = true, bool $excludeBatches = true)
+
+    public function GetStockItemsFull(string $keyWord = "",string $locationId = "",int $entriesPerPage = 100, int $pageNumber = 1, array $dataRequirements = [], array $searchTypes = [],  bool $loadCompositeParents = true, bool $loadVariationParents = true)
     {
-        return $this->get($this->path . '/' . __METHOD__, [
+        if(!empty($dataRequirements)) { $dataRequirements = json_encode($dataRequirements); }
+        if(!empty($searchTypes)) { $searchTypes = json_encode($searchTypes); }
+
+        return $this->get($this->path . '/' . __FUNCTION__, [
             "keyWord" => $keyWord,
             "locationId" => $locationId,
             "entriesPerPage" => $entriesPerPage,
             "pageNumber" => $pageNumber,
-            "excludeComposites" => $excludeComposites,
-            "excludeVariations" => $excludeVariations,
-            "excludeBatches" => $excludeBatches,
+            "loadCompositeParents" => $loadCompositeParents,
+            "loadVariationParents" => $loadVariationParents,
+            "dataRequirements" => $dataRequirements,
+            "searchTypes" => $searchTypes,
         ]);
     }
-    
-    public function getStockItems(string $keyWord = "",string $locationId = "",int $entriesPerPage = 100, int $pageNumber = 1, bool $excludeComposites = true, bool $excludeVariations = true, bool $excludeBatches = true)
+
+    public function GetStockItems(string $keyWord = "",string $locationId = "",int $entriesPerPage = 100, int $pageNumber = 1, bool $excludeComposites = true, bool $excludeVariations = true, bool $excludeBatches = true)
     {
         return $this->get('Stock/GetStockItems', [
             "keyWord" => $keyWord,
